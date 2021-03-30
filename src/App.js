0a92bdbf67ea921511fa8e941fd6945d73ae3fce
myk245 <myk245@nyu.edu>
2021-03-29 23:23:05 -0400

    convert to class component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import styled from 'styled-components';
+import React from 'react';
 
 const AppContainer = styled.div`
   height: 100vh;
@@ -7,35 +8,45 @@ const AppContainer = styled.div`
   padding: 30px;
 `;
 
+const HeadlineContainer = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+`;
+
 const Headline = styled.p`
   font-family: 'Lato', sans-serif;
   color: black;
-  font-size: 35px;
+  font-size: 25px;
   margin-bottom: 30px;
+`;
+
+const LogoContainer = styled.div`
+   height: 70px;
+   width: 60px;
 `;
 
 const AppName = styled.p`
   font-family: 'Lato', sans-serif;
   color: black;
-  font-size: 20px;
+  font-size: 25px;
 `;
 
-const LogoContainer = styled.div`
-   height: 70px;
-   width: 70px;
-`;
-
-function App() {
-  return (
-    <AppContainer>
-      <LogoContainer>
-        <img src="phillies-logo.svg" />
-      </LogoContainer>
-      <Headline>Philadelphia Phillies R&D</Headline>
-      <AppName>Qualifying Offer Calculator</AppName>
-
-    </AppContainer>
-  );
+class App extends React.Component {
+  render() {
+    return(
+      <AppContainer>
+        <HeadlineContainer>
+          <LogoContainer>
+            <img src="phillies-logo.svg" />
+          </LogoContainer>
+          <Headline>Philadelphia Phillies R&D</Headline>
+        </HeadlineContainer>
+        <AppName>Qualifying Offer Calculator</AppName>
+  
+      </AppContainer>
+    );
+  }
 }
 
 export default App;
