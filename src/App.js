524c18fc0917df88e7bcf171cc5827b6b76f3b99
myk245 <myk245@nyu.edu>
2021-03-29 23:16:20 -0400

    basic structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components';
+
+const AppContainer = styled.div`
+  height: 100vh;
+  width: 100vw;
+  padding: 30px;
+`;
+
+const Headline = styled.p`
+  font-family: 'Lato', sans-serif;
+  color: black;
+  font-size: 35px;
+  margin-bottom: 30px;
+`;
+
+const AppName = styled.p`
+  font-family: 'Lato', sans-serif;
+  color: black;
+  font-size: 20px;
+`;
+
+const LogoContainer = styled.div`
+   height: 70px;
+   width: 70px;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppContainer>
+      <LogoContainer>
+        <img src="phillies-logo.svg" />
+      </LogoContainer>
+      <Headline>Philadelphia Phillies R&D</Headline>
+      <AppName>Qualifying Offer Calculator</AppName>
+
+    </AppContainer>
   );
 }
 
