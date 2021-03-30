7237d8dc29a99ece301acf3475427b0e8f9f5e10
myk245 <myk245@nyu.edu>
2021-03-30 02:40:25 -0400

    add resources

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
-# Sources
-1. "Scrape a site with Node and Cheerio in 5 minutes" by Dylan Sather, Medium.com
+# Getting Started
+To get started with this project, please clone this repository and then run:
+'npm start'
+
+The application can then be accessed at 'http://localhost:3000'.
+
+## Libraries Used 
+   - Cheerio: https://github.com/cheeriojs/cheerio
+   - Styled-Components: https://styled-components.com/
+
+## Resources
+- "Scrape a site with Node and Cheerio in 5 minutes" by Dylan Sather, Medium.com
 https://medium.com/@dylan.sather/scrape-a-site-with-node-and-cheerio-in-5-minutes-4617daee3384
--- src/App.js
+++ src/App.js
@@ -2,8 +2,13 @@ import './App.css';
 import styled from 'styled-components';
 import React from 'react';
 
-// to parse HTML
-const cheerio = require('cheerio'); 
+// https://medium.com/@dylan.sather/scrape-a-site-with-node-and-cheerio-in-5-minutes-4617daee3384
+const cheerio = require('cheerio');
+const $ = cheerio.load('https://questionnaire-148920.appspot.com/swe/data.html')
+// isolate player's salary info from table 
+const salaryTable = $('#salary-table')
+// we need to loop through the table and aggregate the top 125 salaries
+// add the top 125 salaries together and then divide by 125 to get value of the qualifying offer
 
 // Styled Components
 const AppContainer = styled.div`
@@ -14,32 +19,41 @@ const AppContainer = styled.div`
 
 const HeadlineContainer = styled.div`
   display: flex;
-  justify-content: flex-start;
+  justify-content: center;
   align-items: center;
 `;
 
 const Headline = styled.p`
   font-family: 'Lato', sans-serif;
   color: black;
-  font-size: 25px;
+  font-size: 50px;
   margin-bottom: 30px;
 `;
 
 const LogoContainer = styled.div`
-   height: 70px;
-   width: 60px;
+   height: 100px;
+   width: 80px;
 `;
 
-const AppName = styled.p`
+const Label = styled.p`
+  position: relative;
+  top: 70px;
   font-family: 'Lato', sans-serif;
   color: black;
   font-size: 25px;
 `;
 
+const QualifyingOfferContainer = styled.div`
+  border: 2px navy dotted;
+  height: 500px;
+  width: 500px;
+  margin: 0 auto;
+  padding: 30px;
+  font-family: 'Lato', sans-serif;
+`;
+
 class App extends React.Component {
-  componentDidMount = () => {
-    const $ = cheerio.load('https://questionnaire-148920.appspot.com/swe/data.html')
-  }
+
 
   render() {
     return(
@@ -50,7 +64,9 @@ class App extends React.Component {
           </LogoContainer>
           <Headline>Philadelphia Phillies R&D</Headline>
         </HeadlineContainer>
-        <AppName>Qualifying Offer Calculator</AppName>
+        <QualifyingOfferContainer>
+          <Label>Upcoming Qualifying Offer:</Label>
+        </QualifyingOfferContainer>
       </AppContainer>
     );
   }
